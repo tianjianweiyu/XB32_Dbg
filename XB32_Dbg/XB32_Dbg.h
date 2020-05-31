8d10449981df3ad2a70cb8254d9b2ab6953e44dc
1770914455@qq.com <1770914455@qq.com>
2020-05-31 18:11:43 +0800

    添加项目文件。

---
+++ XB32_Dbg/XB32_Dbg.h
@@ -0,0 +1,32 @@
+﻿
+// XB32_Dbg.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CXB32DbgApp:
+// 有关此类的实现，请参阅 XB32_Dbg.cpp
+//
+
+class CXB32DbgApp : public CWinApp
+{
+public:
+	CXB32DbgApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CXB32DbgApp theApp;
